77cc13108c1b5111f4153120eee6e58132d62e2d
belgio99 <giovannibellini99@gmail.com>
2021-06-18 20:08:43 +0200

    inserito file api.h

---
+++ include/api.h
@@ -0,0 +1,34 @@
+//
+//  api.h
+//  ProgettoSOL
+//
+//  Created on 13/06/21.
+//
+
+#ifndef api_h
+#define api_h
+
+
+int openConnection(const char* sockname, int msec, const struct timespec abstime);
+
+int closeConnection(const char* sockname);
+
+int openFile(const char* pathname, int flags);
+
+int readFile(const char* pathname, void** buf, size_t* size);
+
+int readNFiles(int n, const char* pathname);
+
+int writeFile(const char* pathname, const char* dirname);
+
+int appendToFile(const char* pathname, void* buf, size_t size, const char* dirname);
+
+int lockFile(const char* pathname);
+
+int unlockFile(const char* pathname);
+
+int closeFile(const char* pathname);
+
+int removeFile(const char* pathname);
+
+#endif /* api_h */
