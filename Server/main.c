8a829325786d7adbb50598adcd70e4b763c468bf
belgio99 <giovannibellini99@gmail.com>
2021-06-19 12:56:10 +0200

    Altra riorganizzazione file

--- Server/main.c
+++ Server/main.c
@@ -20,40 +20,45 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <stdbool.h>
-#include <ctype.h>
+#include "icl_hash.h"
+#include "../utils.h"
 
 #define UNIX_PATH_MAX 104
-#define SOCKNAME "asdac"
-#define N 100
-
 #define MAXFILES 100
 
 typedef struct {
 	int threadnumber;
-	int memorysize;
+	int memorysizemb;
+	int maxfilenumber;
 	char* socketname;
 } Settings;
 
-Settings serversettings = {0,0,NULL};
+typedef struct {
+	int filestorednum;
+	int currentstoragesize;
+	int highestfilenum;
+	int higheststorageused;
+	int filesreplacednum;
+} Stats;
+
+Stats serverstats = {0};
+
+Settings serversettings = {1,100,1000,NULL};
+
+static icl_hash_t* filestorage;
+static int pipefd[2];
+static fd_set set;
 
 void fatalError(char* reason)
 {
-	char* error = "Errore: ";
-	char* str=malloc(strlen(error)+strlen(reason)+2);
-	strcpy(str, error);
+	//char* error = "Errore: ";
+	char* str=malloc(strlen("Errore: ")+strlen(reason)+2);
+	strcpy(str, "Errore: ");
 	perror(strcat(str, reason));
 	free(str);
 	exit(EXIT_FAILURE);
 }
 
-int isNumeric (const char * s)
-{
-	if (s==NULL || *s=='\0' || isspace(*s))
-		return 0;
-	char* p;
-	strtod (s, &p);
-	return *p == '\0';
-}
 
 void socketListener(int sk){
 	int i=0;
@@ -67,9 +72,10 @@ void socketListener(int sk){
 }
 int createServerSocket(void)
 {
+	unlink(serversettings.socketname);
 	struct sockaddr_un sa;
 	sa.sun_family=AF_UNIX;
-	strncpy(sa.sun_path, SOCKNAME,UNIX_PATH_MAX);
+	strncpy(sa.sun_path,serversettings.socketname,UNIX_PATH_MAX);
 	int sk=socket(AF_UNIX,SOCK_STREAM,0);
 	struct sockaddr* test = (struct sockaddr*)&sa;
 	int a = bind(sk,test, sizeof(sa));
@@ -81,7 +87,7 @@ int createServerSocket(void)
 }
 //static int storageFiles = 0;
 //static int storageBytes = 0;
-void readconfig(char* configfilepath)
+void readconfig(const char* configfilepath)
 {
 	FILE* fPtr=fopen(configfilepath, "r");
 	if (fPtr==NULL)
@@ -108,12 +114,23 @@ void readconfig(char* configfilepath)
 			else if (!strcmp(currentsetting, "memorysize")) //da mettere anche max size
 			{
 				if (isNumeric(value))
-					serversettings.memorysize=(int)(long)value;
+					serversettings.memorysizemb=(int)(long)value;
 				else
 				{
 					free(currentsetting);
 					free(value);
 					fatalError("Dimensione della memoria del server non valida!\n");
+				}
+			}
+			else if (!strcmp(currentsetting, "maxfilenumber")) //da mettere anche max size
+			{
+				if (isNumeric(value))
+					serversettings.maxfilenumber=(int)(long)value;
+				else
+				{
+					free(currentsetting);
+					free(value);
+					fatalError("Numero massimo dei file del server non valido!\n");
 				}
 			}
 			else if (!strcmp(currentsetting, "socketname"))
@@ -126,28 +143,30 @@ void readconfig(char* configfilepath)
 		free(currentsetting);
 		free(value);
 	}
-		fclose(fPtr);
-		if (!serversettings.threadnumber)
-		{
-			printf("Non è stato specificato il numero di thread nel file di configurazione, verrà usato 1 come failsafe.\n");
-			serversettings.threadnumber=1;
-		}
-		if (!serversettings.memorysize)
-		{
-			printf("Non è stata specificata la dimensione della memoria del server nel file di configurazione, verranno usati 100 MB come failsafe.\n");
-			serversettings.memorysize=100;
-		}
-		if (serversettings.socketname==NULL)
-			fatalError("Non è stato specificato alcun file del socket!");
-	
-
-	}
+	fclose(fPtr);
+	if (!serversettings.threadnumber)
+		printf("Non è stato specificato il numero di thread nel file di configurazione, verrà usato 1 come failsafe.\n");
+	if (!serversettings.memorysizemb)
+		printf("Non è stata specificata la dimensione della memoria del server nel file di configurazione, verranno usati 100 MB come failsafe.\n");
+	if (!serversettings.maxfilenumber)
+		printf("Non è stato specificato il numero massimo di file memorizzabili nel server. Verrà usato 1000 come failsafe.\n");
+	if (serversettings.socketname==NULL)
+		fatalError("Non è stato specificato alcun file del socket!\n");
+}
 
 
 int main(int argc, const char * argv[]) {
-	char* configfilepath="config.txt";
-	readconfig(configfilepath);
-	
+	if (argc!=2)
+		fatalError("Argomenti non validi! Passare come argomento solo il percorso del file di configurazione!\n");
+	readconfig(argv[2]);
+	filestorage = icl_hash_create(50, NULL, NULL);
+	pthread_t threadids;
+	pipe(pipefd);
+	fd_set tempset;
+	int fdlisten;
+	FD_ZERO(&set);
+	FD_ZERO(&tempset);
+	FD_SET(pipefd[0],&set);
 	pthread_t dispatcher;
 	int sk = createServerSocket();
 	pthread_create(&dispatcher,NULL,&socketListener, sk);
